103694f848d90d31984b32ec857a0875ca18bc13
steindors13 <steindors13@ru.is>
2015-11-26 13:35:32 +0000

    Update main.cpp
    smá start...

--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-using nampespace std;
+struct person;
+void reading_person(vector<person> v);
 
+struct person
+{   string name;
+    string gender;
+    int birthYear;
+    int deathYear;
+};
 int main()
 {
-  cout << "Just checking!!" << endl;
-  return 0;
+    vector<person> v(10);
+    reading_person(v);
+}
+
+void reading_person(vector<person> v)
+{
+    int number;
+    cout << "How many persons? ";
+    cin >> number;
+    cout << endl << "Type in person: " << endl << endl;
+    for(unsigned int i = 0; i < number; i++)
+    {
+        cout << "Name: ";
+        cin >> v[i].name;
+        cout << "Gender: ";
+        cin >> v[i].gender;
+        cout << "Year of birth: ";
+        cin >> v[i].birthYear;
+        cout << "Year of death: ";
+        cin >> v[i].deathYear;
+        cout<< " "<< endl;
+    }
+
+    cout << "Here are your persons: " << endl << endl;
+
+    for(unsigned int i = 0; i < number; i++)
+   {
+        cout << "Name: " << v[i].name << endl;
+        cout << "Gender: " << v[i].gender << endl;
+        cout << "Year of birth: " << v[i].birthYear << endl;
+        cout << "Year of death: " << v[i].deathYear << endl;
+        cout << endl;
+   }
+
 }
