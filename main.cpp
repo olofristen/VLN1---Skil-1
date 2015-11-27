e5d7fc749e6ab5ccd6a5569edf13a7de0c44c420
Ólöf Gyða Risten <olofristen@gmail.com>
2015-11-27 01:06:47 +0000

    Setti inn header og switch

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 struct person;
 void reading_person(vector<person> v);
+void welcome();
+void choices();
 
 struct person
 {   string name;
@@ -11,18 +13,76 @@ struct person
     int birthYear;
     int deathYear;
 };
+
+vector<person> v(10);
+
 int main()
 {
-    vector<person> v(10);
-    reading_person(v);
+    char check = 'y';
+
+    welcome();
+
+    do{
+        choices();
+
+        cout << endl;
+        cout << "Do you wish to continue? (Y/N) " << endl;
+        cin >> check;
+
+    } while(check == 'y' | check == 'Y');
+}
+
+void welcome()
+{
+    cout << "===================================" << endl;
+    cout << "|             WELCOME             |" << endl;
+    cout << "|               to                |" << endl;
+    cout << "|     the Computer Scientist      |" << endl;
+    cout << "|             Database            |" << endl;
+    cout << "===================================" << endl;
+    cout << endl;
+    cout << "In this program you will be able to register and go through the most known"
+         << "computer scientists of all time!" << endl;
+    cout << endl;
+}
+
+void choices()
+{
+    int menu = 0;
+
+    cout << "   =================================================================" << endl;
+    cout << "   |  What do you want to do?                                      |" << endl;
+    cout << "   |    1.  Register new computer scientist/s                      |" << endl;
+    cout << "   |    2.  See all computer scientists registered                 |" << endl;
+    cout << "   |    3.  Search the database                                    |" << endl;
+    cout << "   |  Please enter the number of your choice!                      |" << endl;
+    cout << "   =================================================================" << endl;
+    cin >> menu;
+
+    switch (menu)
+    {
+        case 1:
+            cout << "Register new computer scientist/s" << endl;
+            reading_person(v);
+            break;
+        case 2:
+            cout << "List of all computer scientists registered" << endl;
+            break;
+        case 3:
+            cout << "Search" << endl;
+            break;
+    }
 }
 
 void reading_person(vector<person> v)
 {
     int number;
+
     cout << "How many persons? ";
     cin >> number;
+
     cout << endl << "Type in person: " << endl << endl;
+
     for(unsigned int i = 0; i < number; i++)
     {
         cout << "Name: ";
