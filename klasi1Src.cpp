ed2de3d65ef559fcd01e4e037e826ef57d16a12e
Ólöf Gyða Risten <olofristen@gmail.com>
2015-11-30 11:24:29 +0000

    Added bio

--- klasi1Src.cpp
+++ klasi1Src.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 bool Person::operator < (const Person &r) {
      return this->name < r.name;
- }
+}
 
 bool sortbyyearofbirth (const Person& a, const Person &b) {
     return a.birthYear < b.birthYear;
@@ -13,10 +13,10 @@ bool sortbyyearofbirth (const Person& a, const Person &b) {
 bool sortbygender (const Person& a, const Person &b) {
     return a.gender < b.gender;
 }
+
 bool sortbyyearofdeath (const Person& a, const Person &b) {
     return a.deathYear < b.deathYear;
 }
-
 
 Person::Person()
 {
@@ -24,14 +24,16 @@ Person::Person()
     gender = " ";
     birthYear = 0;
     deathYear = 0;
+    bio = " ";
 }
 
-Person::Person(string name, string gender, int birthYear, int deathYear)
+Person::Person(string name, string gender, int birthYear, int deathYear, string bio)
 {
     this->name = name;
     this->gender = gender;
     this->birthYear = birthYear;
     this->deathYear = deathYear;
+    this->bio = bio;
 }
 
 
@@ -42,29 +44,38 @@ void Person::readData(ifstream& file)
     getline(file, gender);
     getline(file, tempBirth);
     getline(file, tempDeath);
+    getline(file, bio);
     birthYear = atoi(tempBirth.c_str());
     deathYear = atoi(tempDeath.c_str());
-
 }
 
 string Person::getname() {
     return name;
 }
+
 string Person::getgender() {
     return gender;
 }
+
 int Person::getbirthyear() {
     return birthYear;
 }
+
 int Person::getdeathyear() {
     return deathYear;
 }
+
+string Person::getbio(){
+    return bio;
+}
+
 void Person::displayData()
 {
     cout << "Name: " << getname() << endl
     << "Gender: " << getgender() << endl
-    << "Year of birth: " << getbirthyear() << endl
-    << "Year of death: " << getdeathyear() << endl;
+    << "Year of Birth: " << getbirthyear() << endl
+    << "Year of Death: " << getdeathyear() << endl
+    << "Bio: " << getbio() << endl;
 }
 
 void Person::searchData()
@@ -76,4 +87,3 @@ void Person::update()
 {
 
 }
-
