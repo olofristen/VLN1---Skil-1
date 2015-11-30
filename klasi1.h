4596b4c42338897cf8b4068bd26db12208421bd5
steindors13 <steindors13@ru.is>
2015-11-30 10:50:08 +0000

    Create klasi1.h

---
+++ klasi1.h
@@ -0,0 +1,45 @@
+#ifndef KLASI_H
+#define KLASI_H
+
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<cctype>
+#include<cstdlib>
+#include<iomanip>
+#include<algorithm>
+
+using namespace std;
+
+class Person
+{
+
+    public:
+        Person();
+        Person (string name, string gender, int birthYear, int deathYear);
+        void readData(ifstream& file);
+        void displayData();
+        void searchData();
+        void sortData();
+        void update();
+        string getname();
+        string getgender();
+        int getbirthyear();
+        int getdeathyear();
+
+        friend bool sortbyyearofbirth (const Person& a, const Person &b);
+        friend bool sortbyyearofdeath (const Person& a, const Person &b);
+        friend bool sortbygender (const Person& a, const Person &b);
+        bool operator < (const Person& r);
+
+    private:
+        string name;
+        string gender;
+        int birthYear;
+        int deathYear;
+
+
+
+};
+#endif
