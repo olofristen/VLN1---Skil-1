643f4987c9eb16f667a59f2e71e8e4c6627e0321
steindors13 <steindors13@ru.is>
2015-12-03 16:37:08 +0000

    Create computer.cpp

---
+++ computer.cpp
@@ -0,0 +1,65 @@
+#include "computer.h"
+
+bool Computer::operator < (const Computer &r) {
+     return this->name < r.name;
+}
+
+
+computer::computer() {
+    name = " ";
+    buildYear = 0;
+    type = 0;
+    wasBuilt = 0; ";    
+
+}
+
+Computer::Computer(string name, int buildyear, int type, bool wasBuilt)
+{
+    this->name = name;
+    this->buildYear = buildyear;
+    this->type = type;
+    this->wasBuilt = wasBuilt;
+}
+
+
+string Computer::getname() {
+    return name;
+}
+int Computer::getbuildyear() {
+    return birthYear;
+}
+int Computer::gettype() {
+    return type;
+}
+bool Computer::getwasbuilt() {
+    return wasBuilt;
+}
+
+void Computer::displayData()
+{
+    cout << "Name: " << getname() << endl
+         << "Made in: " << getbuildyear() << endl
+         << "Type: " << gettype() << endl;
+    if(getwasbuilt() == -1) {
+        cout << "This computer was only a thought!" << endl;
+    }
+    else {
+        cout << "Yes, this computer was built. " << endl;
+    }
+         << "------------------------------------------------------------------" << endl;
+}
+
+/*void Computer::readData(ifstream& file)
+{
+    string tempBuilt, tempType, tempWasbuilt;
+    getline(file, name);
+    getline(file, tempBuilt);
+    getline(file, tempType);
+    getline(file, wasBuilt);
+    builtYear = atoi(tempBuilt.c_str());
+    type = atoi(tempType.c_str());
+    wasBuilt = atoi(tempWasbuilt.c_str());
+}*/
+
+
+
